b56662479474740c9d77d1652928397273257810
Ryan Kiptoo <waldoryan4@gmail.com>
2025-03-02 00:35:37 +0300

    user profile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserProfile from '. /UserProfile'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,8 @@ function App() {
   return (
     
     <>
+    <h1>UserProfile</h1>
+    <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     <Header />
     <MainContent />
     <Footer />
