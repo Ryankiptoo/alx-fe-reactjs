a56ad60f014ffc66eb4cf685ac69e274ec4d5bc0
Ryan Kiptoo <waldoryan4@gmail.com>
2025-03-16 23:35:12 +0300

    recipe list

--- recipe-sharing-app/src/components/RecipeStore.js
+++ recipe-sharing-app/src/components/RecipeStore.js
@@ -1,7 +1,8 @@
 import create from 'zustand'
 
-const userecipeStore = create(set => ({
+const recipeStore = create(set => ({
   recipes: [],
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes })
 }));
+export default recipeStore;
