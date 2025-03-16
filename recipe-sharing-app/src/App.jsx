a1768bb3df12433a2e33530b66157fb3f5cfc904
Ryan Kiptoo <waldoryan4@gmail.com>
2025-03-16 23:26:11 +0300

    recipe list

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import recipeList from './components/RecipeList';
+import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
     <>
 
 <AddRecipeForm />
-      <recipeList />
+      <RecipeList />
 
       <div>
         <a href="https://vite.dev" target="_blank">
